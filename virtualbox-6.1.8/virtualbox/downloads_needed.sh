1c0a92afa6f0f9ea8a147dbf12b32b6a2703e800
rich <rhintze@gmail.com>
2020-05-28 13:43:29 -0400

    bumped to 6.1.8

---
+++ virtualbox-6.1.8/virtualbox/downloads_needed.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+wget https://download.virtualbox.org/virtualbox/6.1.8/VBoxGuestAdditions_6.1.8.iso
+wget https://download.virtualbox.org/virtualbox/6.1.8/VirtualBox-6.1.8.tar.bz2
+wget http://download.virtualbox.org/virtualbox/6.1.8/UserManual.pdf
+wget http://download.virtualbox.org/virtualbox/6.1.8/SDKRef.pdf

4b4b79b9e68ce62fb8512a0d42c6fa967e7ff1b9
Richard Hintze <rich@lehcar.duckdns.org>
2020-07-29 19:30:31 -0400

    upgraded to 6.1.2 thanks to upnort

--- virtualbox-6.1.12/virtualbox-extension-pack/cleanup_for_post.sh
+++ virtualbox-6.1.12/virtualbox-extension-pack/cleanup_for_post.sh
@@ -1,1 +1,1 @@
-rm ./Oracle_VM_VirtualBox_Extension_Pack-6.1.10.vbox-extpack
+rm ./Oracle_VM_VirtualBox_Extension_Pack-6.1.12.vbox-extpack
--- virtualbox-6.1.12/virtualbox/downloads_needed.sh
+++ virtualbox-6.1.12/virtualbox/downloads_needed.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-wget https://download.virtualbox.org/virtualbox/6.1.10/VBoxGuestAdditions_6.1.10.iso
-wget https://download.virtualbox.org/virtualbox/6.1.10/VirtualBox-6.1.10.tar.bz2
-wget http://download.virtualbox.org/virtualbox/6.1.10/UserManual.pdf
-wget http://download.virtualbox.org/virtualbox/6.1.10/SDKRef.pdf
+wget https://download.virtualbox.org/virtualbox/6.1.12/VBoxGuestAdditions_6.1.12.iso
+wget https://download.virtualbox.org/virtualbox/6.1.12/VirtualBox-6.1.12.tar.bz2
+wget http://download.virtualbox.org/virtualbox/6.1.12/UserManual.pdf
+wget http://download.virtualbox.org/virtualbox/6.1.12/SDKRef.pdf

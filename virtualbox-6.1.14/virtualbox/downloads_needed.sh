c648666457628e3eef6a3995d2096ac1b21111e3
Richard Hintze <rich@lehcar.duckdns.org>
2020-09-07 16:06:18 -0400

    initial 6.1.14 branch

---
+++ virtualbox-6.1.14/virtualbox/downloads_needed.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+wget https://download.virtualbox.org/virtualbox/6.1.12/VBoxGuestAdditions_6.1.12.iso
+wget https://download.virtualbox.org/virtualbox/6.1.12/VirtualBox-6.1.12.tar.bz2
+wget http://download.virtualbox.org/virtualbox/6.1.12/UserManual.pdf
+wget http://download.virtualbox.org/virtualbox/6.1.12/SDKRef.pdf

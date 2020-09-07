8c5ce94c5219e79d328f349bf34ad09472882f05
Richard Hintze <rich@lehcar.duckdns.org>
2020-09-07 16:16:07 -0400

    ready to test 6.1.14

--- virtualbox-6.1.14/virtualbox/cleanup_for_post.sh
+++ virtualbox-6.1.14/virtualbox/cleanup_for_post.sh
@@ -1,4 +1,4 @@
-rm ./VirtualBox-6.1.12.tar.bz2
+rm ./VirtualBox-6.1.14.tar.bz2
 rm ./SDKRef.pdf
 rm ./UserManual.pdf
-rm ./VBoxGuestAdditions_6.1.12.iso
+rm ./VBoxGuestAdditions_6.1.14.iso
--- virtualbox-6.1.14/virtualbox/downloads_needed.sh
+++ virtualbox-6.1.14/virtualbox/downloads_needed.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-wget https://download.virtualbox.org/virtualbox/6.1.12/VBoxGuestAdditions_6.1.12.iso
-wget https://download.virtualbox.org/virtualbox/6.1.12/VirtualBox-6.1.12.tar.bz2
-wget http://download.virtualbox.org/virtualbox/6.1.12/UserManual.pdf
-wget http://download.virtualbox.org/virtualbox/6.1.12/SDKRef.pdf
+wget https://download.virtualbox.org/virtualbox/6.1.14/VBoxGuestAdditions_6.1.14.iso
+wget https://download.virtualbox.org/virtualbox/6.1.14/VirtualBox-6.1.14.tar.bz2
+wget http://download.virtualbox.org/virtualbox/6.1.14/UserManual.pdf
+wget http://download.virtualbox.org/virtualbox/6.1.14/SDKRef.pdf

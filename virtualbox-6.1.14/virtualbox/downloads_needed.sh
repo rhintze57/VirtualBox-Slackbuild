1cdee586f79fdb3618fbdde9d596f229ef59baac
ricky_cardo <rhintze@gmail.com>
2020-09-12 12:01:44 -0400

    using version 6.1.14a allowing VirtIO

--- virtualbox-6.1.14/virtualbox/downloads_needed.sh
+++ virtualbox-6.1.14/virtualbox/downloads_needed.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
 wget https://download.virtualbox.org/virtualbox/6.1.14/VBoxGuestAdditions_6.1.14.iso
-wget https://download.virtualbox.org/virtualbox/6.1.14/VirtualBox-6.1.14.tar.bz2
+wget https://download.virtualbox.org/virtualbox/6.1.14/VirtualBox-6.1.14a.tar.bz2
 wget http://download.virtualbox.org/virtualbox/6.1.14/UserManual.pdf
 wget http://download.virtualbox.org/virtualbox/6.1.14/SDKRef.pdf

e5c3cf610f192abcdc405a4b147ebc243d2365de
ricky_cardo <rhintze@gmail.com>
2021-12-02 08:47:23 -0500

    updated to new virtualbox release 6.1.30

---
+++ virtualbox-6.1.30/virtualbox/downloads_needed.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+wget https://download.virtualbox.org/virtualbox/6.1.30/VBoxGuestAdditions_6.1.30.iso
+wget https://download.virtualbox.org/virtualbox/6.1.30/VirtualBox-6.1.30.tar.bz2
+wget http://download.virtualbox.org/virtualbox/6.1.30/UserManual.pdf
+wget http://download.virtualbox.org/virtualbox/6.1.30/SDKRef.pdf

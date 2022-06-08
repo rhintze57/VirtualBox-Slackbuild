85b931ce27e08dcce810afa2829d37d6c288dbdd
Richard Hintze <rhintze@gmail.com>
2022-06-07 22:16:51 -0400

    bumped to 6.1.34, untested

--- virtualbox-6.1.32/virtualbox/cleanup_for_post.sh
+++
@@ -1,4 +0,0 @@
-rm ./VirtualBox-6.1.32.tar.bz2
-rm ./SDKRef.pdf
-rm ./UserManual.pdf
-rm ./VBoxGuestAdditions_6.1.32.iso
---
+++ virtualbox-6.1.34/virtualbox-extension-pack/cleanup_for_post.sh
@@ -0,0 +1,1 @@
+rm ./Oracle_VM_VirtualBox_Extension_Pack-6.1.34.vbox-extpack

cc4b70026713cce5e685a534a8318fc074289bc2
Rich Hintze <rhintze@gmail.com>
2022-11-28 19:00:47 -0500

    bumped to 6.1.40 still to be tested

--- virtualbox-6.1.38/virtualbox/downloads_needed.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-wget https://download.virtualbox.org/virtualbox/6.1.38/VBoxGuestAdditions_6.1.38.iso
-wget https://download.virtualbox.org/virtualbox/6.1.38/VirtualBox-6.1.38.tar.bz2
-wget http://download.virtualbox.org/virtualbox/6.1.38/UserManual.pdf
-wget http://download.virtualbox.org/virtualbox/6.1.38/SDKRef.pdf

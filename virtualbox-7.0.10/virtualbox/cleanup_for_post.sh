e474093f8e8ff658d5f3b84df988d10dd37bc5f5
Rich Hintze <rhintze@gmail.com>
2023-07-26 17:21:29 -0400

    initial 7.0.10

---
+++ virtualbox-7.0.10/virtualbox/cleanup_for_post.sh
@@ -0,0 +1,4 @@
+rm ./VirtualBox-7.0.10.tar.bz2
+rm ./SDKRef.pdf
+rm ./UserManual.pdf
+rm ./VBoxGuestAdditions_7.0.10.iso
---
+++ virtualbox-7.0.10/virtualbox/downloads_needed.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+wget https://download.virtualbox.org/virtualbox/7.0.10/VBoxGuestAdditions_7.0.10.iso
+wget https://download.virtualbox.org/virtualbox/7.0.10/VirtualBox-7.0.10.tar.bz2
+wget http://download.virtualbox.org/virtualbox/7.0.10/UserManual.pdf
+wget http://download.virtualbox.org/virtualbox/7.0.10/SDKRef.pdf

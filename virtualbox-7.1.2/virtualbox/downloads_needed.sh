487afb66cc4d451d5ca10a9a02bc7d7c26df5f9e
Rich Hintze <rhintze@gmail.com>
2024-10-07 18:34:57 -0400

    updated to 7.1.2

---
+++ virtualbox-7.1.2/virtualbox/downloads_needed.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+wget https://download.virtualbox.org/virtualbox/7.1.2/VBoxGuestAdditions_7.1.2.iso
+wget https://download.virtualbox.org/virtualbox/7.1.2/VirtualBox-7.1.2.tar.bz2
+wget http://download.virtualbox.org/virtualbox/7.1.2/UserManual.pdf
+wget http://download.virtualbox.org/virtualbox/7.1.2/SDKRef.pdf

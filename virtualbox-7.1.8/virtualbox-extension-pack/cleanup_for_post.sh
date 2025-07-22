829a6196c94ccdfc3f462fa670233cf731955b0c
Rich Hintze <rhintze@gmail.com>
2025-07-21 22:13:31 -0400

    version 7.1.12

--- virtualbox-7.1.8/virtualbox-extension-pack/cleanup_for_post.sh
+++
@@ -1,1 +0,0 @@
-rm ./Oracle_VirtualBox_Extension_Pack-7.1.8.vbox-extpack
--- virtualbox-7.1.8/virtualbox-extension-pack/downloads_needed.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-
-wget https://download.virtualbox.org/virtualbox/7.1.8/Oracle_VirtualBox_Extension_Pack-7.1.8.vbox-extpack

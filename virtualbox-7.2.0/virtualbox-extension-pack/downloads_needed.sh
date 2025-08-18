fe80116397f3c2bd2128aefede03a5ec9b0d6c4a
Rich Hintze <rhintze@gmail.com>
2025-08-17 21:53:47 -0400

    seems ok

---
+++ virtualbox-7.2.0/virtualbox-extension-pack/downloads_needed.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+wget https://download.virtualbox.org/virtualbox/7.2.0/Oracle_VirtualBox_Extension_Pack-7.2.0.vbox-extpack
---
+++ virtualbox-7.2.0/virtualbox/downloads_needed.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+wget https://download.virtualbox.org/virtualbox/7.2.0/VBoxGuestAdditions_7.2.0.iso
+wget https://download.virtualbox.org/virtualbox/7.2.0/VirtualBox-7.2.0.tar.bz2
+wget http://download.virtualbox.org/virtualbox/7.2.0/UserManual.pdf
+wget http://download.virtualbox.org/virtualbox/7.2.0/SDKRef.pdf

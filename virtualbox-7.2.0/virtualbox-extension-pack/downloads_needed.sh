d2010044418950fb74842212b848c29ae6240a86
Rich Hintze <rhintze@gmail.com>
2025-09-28 16:00:41 -0400

    bumped to 7.2.2

--- virtualbox-7.2.0/virtualbox-extension-pack/downloads_needed.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-
-wget https://download.virtualbox.org/virtualbox/7.2.0/Oracle_VirtualBox_Extension_Pack-7.2.0.vbox-extpack
---
+++ virtualbox-7.2.2/virtualbox/downloads_needed.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+wget https://download.virtualbox.org/virtualbox/7.2.2/VBoxGuestAdditions_7.2.2.iso
+wget https://download.virtualbox.org/virtualbox/7.2.2/VirtualBox-7.2.2.tar.bz2
+wget http://download.virtualbox.org/virtualbox/7.2.2/UserManual.pdf
+wget http://download.virtualbox.org/virtualbox/7.2.2/SDKRef.pdf

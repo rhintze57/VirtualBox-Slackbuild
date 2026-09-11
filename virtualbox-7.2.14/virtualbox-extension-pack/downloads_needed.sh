98f5a24cc1503d2661574fb09f6894a0e20cdfdf
Rich Hintze <rhintze@gmail.com>
2026-09-11 15:56:50 -0400

    initial 7.2.16

--- virtualbox-7.2.14/virtualbox-extension-pack/downloads_needed.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-
-wget https://download.virtualbox.org/virtualbox/7.2.14/Oracle_VirtualBox_Extension_Pack-7.2.14.vbox-extpack
---
+++ virtualbox-7.2.16/virtualbox/downloads_needed.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+wget https://download.virtualbox.org/virtualbox/7.2.16/VBoxGuestAdditions_7.2.16.iso
+wget https://download.virtualbox.org/virtualbox/7.2.16/VirtualBox-7.2.16.tar.bz2
+wget http://download.virtualbox.org/virtualbox/7.2.16/UserManual.pdf
+wget http://download.virtualbox.org/virtualbox/7.2.16/SDKRef.pdf
